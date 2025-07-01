390951e053d53a0aefc01f4c8a9d02cb831cacae
Erzis900 <erzis900@gmail.com>
2025-07-01 20:17:11 +0200

    safe read and write functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,11 +3,36 @@
 
 namespace Utils
 {
+	template<typename T>
+	T Read(DWORD address)
+	{
+		DWORD oldProtect;
+		VirtualProtect((LPVOID*)address, sizeof(T), PAGE_EXECUTE_READWRITE, &oldProtect);
+
+		T value = *(T*)(address);
+
+		VirtualProtect((LPVOID*)address, sizeof(T), oldProtect, &oldProtect);
+
+		return value;
+	}
+
+	template<typename T>
+	void Write(DWORD address, const T& value)
+	{
+		DWORD oldProtect;
+
+		VirtualProtect((LPVOID*)address, sizeof(T), PAGE_EXECUTE_READWRITE, &oldProtect);
+
+		*(T*)(address) = value;
+
+		VirtualProtect((LPVOID*)address, sizeof(T), oldProtect, &oldProtect);
+	}
+
 	DWORD ResolveChain(DWORD base, std::vector<DWORD> offsets)
 	{
 		for (auto& o : offsets)
-		{
-			base = *(DWORD*)base;
+		{			
+			base = Read<DWORD>(base);
 			base += o;
 		}
 
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,12 @@
 
 namespace Utils
 {
+	template<typename T>
+	T Read(DWORD address);
+
+	template<typename T>
+	void Write(DWORD address, const T& value);
+
 	DWORD ResolveChain(DWORD base, std::vector<DWORD> offsets);
 	void Patch(DWORD address, std::vector<BYTE> bytes);
 	void NOP(DWORD address, int count);
